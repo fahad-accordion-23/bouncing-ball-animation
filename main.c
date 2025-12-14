9987ff705ba3c79ec4d30d5686c06bf26c13f42f
Fahad Hassan <fahad.1979.hassan@gmail.com>
2025-12-14 22:23:18 +0500

    More refactoring

--- main.c
+++ main.c
@@ -7,63 +7,87 @@
 #define COLS 50
 #define ROWS 20
 
-char **init_buffer(uint32_t cols, uint32_t rows);
-void clear_buffer(char** buffer, uint32_t cols, uint32_t rows);
-void display_buffer(char** buffer, uint32_t cols, uint32_t rows);
+typedef struct {
+    char** data;
+    uint32_t cols;
+    uint32_t rows;
+} Buffer;
 
-void draw_circle(char** buffer, uint32_t radius);
-void draw_ellipse(char** buffer, uint32_t x_axis, uint32_t y_axis);
+void buffer_init(Buffer* buffer, uint32_t cols, uint32_t rows);
+void buffer_clear(Buffer* buffer);
+void buffer_display(Buffer* buffer);
+void buffer_free(Buffer* buffer);
+
+void draw_circle(Buffer* buffer, uint32_t radius);
+void draw_ellipse(Buffer* buffer, uint32_t x_axis, uint32_t y_axis);
 
 int main(int argc, char** argv)
 {
-    char** display = init_buffer(COLS, ROWS); 
-    
+    Buffer display = { 0 };
+    buffer_init(&display, COLS, ROWS);
+
     while (1)
     {
-        clear_buffer(display, COLS, ROWS); 
-        
-        draw_ellipse(display, 10, 5);
+        buffer_clear(&display);
 
-        display_buffer(display, COLS, ROWS);
+        draw_ellipse(&display, 10, 5);
+
+        buffer_display(&display);
+
+        usleep(50 * 1000);
     }
+
+    buffer_free(&display);
 
     return 0;
 }
 
-char** init_buffer(uint32_t cols, uint32_t rows)
+void buffer_init(Buffer* buffer, uint32_t cols, uint32_t rows)
 {
-    char** buffer = malloc(ROWS * sizeof(char*));
-    for (int i = 0; i < ROWS; i++)
-    {
-        buffer[i] = malloc(COLS * sizeof(char));
-        memset(buffer[i], ' ', COLS * sizeof(char));
-    }
-
-    return buffer;
-}
-
-void clear_buffer(char** buffer, uint32_t cols, uint32_t rows)
-{
+    buffer->data = malloc(rows * sizeof(char*));
     for (int i = 0; i < rows; i++)
     {
-        memset(buffer[i], ' ', cols * sizeof(char));
+        buffer->data[i] = malloc(cols * sizeof(char));
+        memset(buffer->data[i], ' ', cols * sizeof(char));
+    }
+
+    buffer->cols = cols;
+    buffer->rows = rows;
+}
+
+void buffer_clear(Buffer* buffer)
+{
+    for (int i = 0; i < buffer->rows; i++)
+    {
+        memset(buffer->data[i], ' ', buffer->cols * sizeof(char));
     }
 }
 
-void display_buffer(char** buffer, uint32_t cols, uint32_t rows)
+void buffer_display(Buffer* buffer)
 {
     printf("\033[H");
 
-    for(int i = 0; i < ROWS; i++)
+    for(int i = 0; i < buffer->rows; i++)
     {
-        fwrite(buffer[i], 1, COLS, stdout);
+        fwrite(buffer->data[i], 1, buffer->cols, stdout);
         printf("\n");
     }
 
     fflush(stdout);
 }
 
-void draw_circle(char** buffer, uint32_t radius)
+void buffer_free(Buffer* buffer)
+{
+    for (int i = 0; i < buffer->rows; i++)
+        free(buffer->data[i]);
+    free(buffer->data);
+
+    buffer->data = NULL;
+    buffer->cols = 0;
+    buffer->rows = 0;
+}
+
+void draw_circle(Buffer* buffer, uint32_t radius)
 {
     uint32_t center_x = radius - 1;
     uint32_t center_y = radius - 1;
@@ -78,12 +102,12 @@ void draw_circle(char** buffer, uint32_t radius)
                                        + ((i - center_x) * (i - center_x));
 
             if (distance_squared <= radius_squared)
-                buffer[j][i] = '*';
+                buffer->data[j][i] = '*';
         }
     }
 }
 
-void draw_ellipse(char** buffer, uint32_t x_axis, uint32_t y_axis)
+void draw_ellipse(Buffer* buffer, uint32_t x_axis, uint32_t y_axis)
 {
     uint32_t center_x = x_axis;
     uint32_t center_y = y_axis;
@@ -106,7 +130,7 @@ void draw_ellipse(char** buffer, uint32_t x_axis, uint32_t y_axis)
             uint32_t y_squared = (j - center_y) * (j - center_y);
 
             if ((x_squared * y_axis_squared) + (y_squared * x_axis_squared) <= (x_axis_squared * y_axis_squared))
-                buffer[j][i] = '*';
+                buffer->data[j][i] = '*';
         }
     }
 }
